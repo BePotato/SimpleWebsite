83547196c8c5039f7c937ec533651c49fba366c4
Max <maximilian.senten00@gmail.com>
2021-06-02 15:46:27 +0200

    bool op true

--- BouwDataWebApp/src/environments/environment.ts
+++ BouwDataWebApp/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyAx0ieLbdeFiVRlZNgYIpWNSIfoY2matBc",
     authDomain: "bouwdatadb.firebaseapp.com",
