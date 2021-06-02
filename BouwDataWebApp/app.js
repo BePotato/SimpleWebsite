353d4e8837849f290ddb466e679540d031f42534
Max <maximilian.senten00@gmail.com>
2021-06-02 14:31:05 +0200

    port change

--- BouwDataWebApp/app.js
+++ BouwDataWebApp/app.js
@@ -2,7 +2,7 @@ var express = require('express')
   , app = express()
   , server = require('http').createServer(app);
 
-server.listen(3000);
+server.listen(8082);
 
 app.get('/', function(req, res) {
   res.send("Hello prod!");
